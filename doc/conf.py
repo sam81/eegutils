dd27778546e20baa8be4543bcc07dfc2af4fdef3
Samuele Carcagno <sam.carcagno@gmail.com>
2014-03-05 00:19:54 +0000

    sys.path for building docs

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -48,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'eegutils'
-copyright = u'2013, Samuele Carcagno'
+copyright = u'2013-2014, Samuele Carcagno'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
